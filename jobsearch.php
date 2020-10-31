9d7a1b95f32961fa3ae748ca7f7c7cef6dd83239
shashank-dotcom <73489321+shashank-dotcom@users.noreply.github.com>
2020-10-31 22:50:48 +0530

    Add files via upload

---
+++ jobsearch.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html >
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>JOB SEARCH</title>
+<link rel="stylesheet" type="text/css" href="view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
+
+</head>
+<body id="main_body" >
+	
+	<img id="top" src="top.png" alt="">
+	<div id="form_container">
+	
+		<h1><a>JOB SEARCH</a></h1>
+		<form id="form_4892" class="appnitro"  method="post" action="">
+					<div class="form_description">
+			<h2>JOB SEARCH</h2>
+			<p></p>
+		</div>						
+			<ul >
+			
+					<li id="li_1" >
+		<label class="description" for="element_1">SKILLS </label>
+		<div>
+		<select class="element select medium" id="element_1" name="element_1"> 
+			<option value="" selected="selected"></option>
+<option value="1" >C,C++</option>
+<option value="2" >JAVA</option>
+<option value="3" >PHP</option>
+<option value="4" >ANDROID</option>
+<option value="5" >TESTING</option>
+
+		</select>
+		</div> 
+		</li>		<li id="li_2" >
+		<label class="description" for="element_2">LOCATION </label>
+		<div>
+		<select class="element select medium" id="element_2" name="element_2"> 
+			<option value="" selected="selected"></option>
+<option value="1" >BANGLORE</option>
+<option value="2" >GURGAON</option>
+<option value="3" >DELHI</option>
+<option value="4" >ANAND VIHAR</option>
+<option value="5" >LUCKNOW</option>
+<option value="6" >JALANDHAR</option>
+
+		</select>
+		</div> 
+		</li>
+			
+					<li class="buttons">
+			    <input type="hidden" name="form_id" value="4892" />
+			    
+				<input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
+		</li>
+			</ul>
+		</form>	
+		
+	</div>
+	<img id="bottom" src="bottom.png" alt="">
+	</body>
+</html>

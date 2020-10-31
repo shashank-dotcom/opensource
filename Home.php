119b36b7ba9458bcb517d6a031eb4011564192d4
shashank-dotcom <73489321+shashank-dotcom@users.noreply.github.com>
2020-10-31 22:52:16 +0530

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>SHASHANK JOB PORTAL</title>
+<link rel="stylesheet" type="text/css" href="view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
+
+</head>
+<body id="main_body" >
+	
+	<img id="top" src="top.png" alt="">
+	<div id="form_container">
+	
+		<h1><a>SHASHANK JOB PORTAL</a></h1>
+		<form id="form_4899" class="appnitro" enctype="multipart/form-data" method="post" action="">
+					<div class="form_description">
+			<h2>SHASHANK JOB PORTAL</h2>
+			<p>BUILD YOUR DREAM JOB HERE</p>
+		</div>						
+			<ul >
+			
+					<li id="li_1" >
+		<label class="description" for="element_1">NAME </label>
+		<span>
+			<input id="element_1_1" name= "element_1_1" class="element text" maxlength="255" size="8" value=""/>
+			<label>First</label>
+		</span>
+		<span>
+			<input id="element_1_2" name= "element_1_2" class="element text" maxlength="255" size="14" value=""/>
+			<label>Last</label>
+		</span> 
+		</li>		<li id="li_2" >
+		<label class="description" for="element_2">EMAIL </label>
+		<div>
+			<input id="element_2" name="element_2" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_5" >
+		<label class="description" for="element_5">SKILLS </label>
+		<div>
+		<select class="element select medium" id="element_5" name="element_5"> 
+			<option value="" selected="selected"></option>
+<option value="1" >C,C++</option>
+<option value="2" >JAVA</option>
+<option value="3" >PYTHON</option>
+<option value="4" >ANDROID</option>
+<option value="5" >R</option>
+<option value="6" >PHP</option>
+
+		</select>
+		</div> 
+		</li>		<li id="li_3" >
+		<label class="description" for="element_3">PHONE </label>
+		<span>
+			<input id="element_3_1" name="element_3_1" class="element text" size="3" maxlength="3" value="" type="text"> -
+			<label for="element_3_1">(###)</label>
+		</span>
+		<span>
+			<input id="element_3_2" name="element_3_2" class="element text" size="3" maxlength="3" value="" type="text"> -
+			<label for="element_3_2">###</label>
+		</span>
+		<span>
+	 		<input id="element_3_3" name="element_3_3" class="element text" size="4" maxlength="4" value="" type="text">
+			<label for="element_3_3">####</label>
+		</span>
+		 
+		</li>		<li id="li_4" >
+		<label class="description" for="element_4">UPLOAD CV </label>
+		<div>
+			<input id="element_4" name="element_4" class="element file" type="file"/> 
+		</div>  
+		</li>		<li id="li_6" >
+		<label class="description" for="element_6">GENDER </label>
+		<span>
+			<input id="element_6_1" name="element_6_1" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_6_1">MALE</label>
+<input id="element_6_2" name="element_6_2" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_6_2">FEMALE</label>
+
+		</span> 
+		</li>
+			
+					<li class="buttons">
+			    <input type="hidden" name="form_id" value="4899" />
+			    
+				<input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
+		</li>
+			</ul>
+		</form>	
+	
+	</div>
+	<img id="bottom" src="bottom.png" alt="">
+	</body>
+</html>
